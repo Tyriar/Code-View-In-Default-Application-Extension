fdba650630d568663edd0b33cc4c7b3b16d922c2
Sidney Andrews <sidney@seesharprun.net>
2016-03-02 17:21:01 -0500

    Updated TypeScript

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,44 @@
 'use strict';
 
 import * as vscode from 'vscode';
+
 var open = require('open');
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "view-in-default-application" is now active!');
-
-	let disposable = vscode.commands.registerCommand('extension.viewInDefaultApp', () => {
-		var editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
-        }
-        var fileName = editor.document.fileName;
-        open(fileName);
-	});
-
-	context.subscriptions.push(disposable);
+              
+    let disposable = vscode.commands.registerCommand('extension.viewInDefaultApp', () => {
+        new ApplicationLauncher().showInDefaultApp();
+    });
+    
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
+    
+}
+
+class ApplicationLauncher {
+    
+    _editor: vscode.TextEditor;
+    
+    constructor() {
+        
+        this._editor = vscode.window.activeTextEditor;
+        
+    }
+    
+    showInDefaultApp() {
+        
+        if (!this._editor) {
+            
+            return; // No open text editor
+            
+        }
+        
+        var fileName = this._editor.document.fileName;
+        
+        open(fileName);
+	}
 }
